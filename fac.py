428b56bb64c0846f345ed097e975f263cd4a6efc
Santhosh Kumar Srinivasan <san@sanspace.in>
2017-10-16 21:34:13 -0400

    initial commit

---
+++ fac.py
@@ -0,0 +1,12 @@
+def factorial(n):
+  print("factorial of ", n)
+  while (n > 1):
+    t = n * factorial(n - 1)
+    print("returning ", t)
+    return t
+  print("returning ", 1)
+  return 1
+
+print(factorial(4))
+
+
